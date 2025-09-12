2d7e7f373c5cd12455472788a1f3d60ec2cd7b1f
arnavchevula <nagarjunchevula@Nagarjuns-MBP-2.attlocal.net>
2025-09-12 02:11:45 -0500

    added cards for now playing page

--- src/components/NowPlaying.jsx
+++ src/components/NowPlaying.jsx
@@ -2,18 +2,102 @@ import useNowPlaying from "../hooks/useNowPlaying";
 
 function NowPlaying({}) {
   const [musicians, setMusicians] = useNowPlaying();
-  console.log(musicians);
-
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center">
       {musicians.map(musician => {
         return (
-          <div>
-            <div>{musician.name}</div>
-            <div>{musician.instrument}</div>
-            <div>{musician.instagram}</div>
-            <div>{musician.phoneNumber}</div>
-            <div>{musician.email}</div>
+          <div
+            key={musician._id}
+            class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 mt-2"
+          >
+            <div class="flex justify-end px-4 pt-4">
+              <button
+                id="dropdownButton"
+                data-dropdown-toggle="dropdown"
+                class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5"
+                type="button"
+              >
+                <span class="sr-only">Open dropdown</span>
+                <svg
+                  class="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 16 3"
+                >
+                  <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+                </svg>
+              </button>
+              <div
+                id="dropdown"
+                class="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700"
+              >
+                <ul class="py-2" aria-labelledby="dropdownButton">
+                  <li>
+                    <a
+                      href="#"
+                      class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    >
+                      Edit
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    >
+                      Export Data
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    >
+                      Delete
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="flex flex-col items-center pb-10">
+              <div class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                <span class="font-medium text-gray-600 dark:text-gray-300">
+                  {musician.name.split(" ")[0][0]}
+                  {musician.name.split(" ")[1][0]}
+                </span>
+              </div>
+              <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                {musician.name}
+              </h5>
+              <span class="text-sm text-gray-500 dark:text-gray-400">
+                {musician.instrument}
+              </span>
+              <span class="text-sm text-gray-500 dark:text-gray-400">
+                {musician.instagram}
+              </span>
+              <span class="text-sm text-gray-500 dark:text-gray-400">
+                {musician.phoneNumber}
+              </span>
+              <span class="text-sm text-gray-500 dark:text-gray-400">
+                {musician.email}
+              </span>
+
+              <div class="flex mt-4 md:mt-6">
+                <a
+                  href="#"
+                  class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Add friend
+                </a>
+                <a
+                  href="#"
+                  class="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                >
+                  Message
+                </a>
+              </div>
+            </div>
           </div>
         );
       })}
